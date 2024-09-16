b33becfb367a5e200c03b30cb3e75e40b8bac553
k <kyle.offenbacher@gmail.com>
2024-09-15 22:54:50 -0400

    Add files via upload

---
+++ inventory_management.js
@@ -0,0 +1,34 @@
+//coding challenge 4
+
+const inventory = [
+    {
+        name: 'computer',
+        price: 1000,
+        quantity: 14,
+        lowStockLevel: 5
+      },
+      {
+        name: 'phone',
+        price: 800,
+        quantity: 15,
+        lowStockLevel: 7
+      },
+      {
+        name: 'headphones',
+        price: 200,
+        quantity: 22,
+        lowStockLevel: 8
+      },
+      {
+        name: 'smart watch',
+        price: 350,
+        quantity: 12,
+        lowStockLevel: 5
+      },
+      {
+        name: 'tablet',
+        price: 600,
+        quantity: 16,
+        lowStockLevel: 8
+      }
+    ];
